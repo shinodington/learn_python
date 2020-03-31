298b6b828392e78446a5a4de0b6fa71e1a877b5b
Mullov Yury <shinodington@yandex.ru>
2020-03-31 21:01:24 +0300

    complet task 2 bubbles.py

--- 00_bubbles.py
+++ 00_bubbles.py
@@ -6,16 +6,23 @@ sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
 
-point = sd.get_point(x = 100, y = 100)
-radius = 50
-for _ in range(3):
-    step = 5
-    radius += step
-    sd.circle(point, radius=radius)
+#point = sd.get_point(x = 100, y = 100)
+#radius = 50
+#for _ in range(3):
+#    step = 5
+#    radius += step
+#    sd.circle(point, radius=radius)
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 # TODO здесь ваш код
+def puz_param(point, step):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(point, radius=radius)
 
+point = sd.get_point(x = 100, y = 100)
+puz_param(point = point, step = 5)
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
 
