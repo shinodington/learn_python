44087bb543a95cd470467b492042288f0c76c883
Mullov Yury <shinodington@yandex.ru>
2020-03-31 21:09:22 +0300

    complete task 4 bubbles.py

--- 00_bubbles.py
+++ 00_bubbles.py
@@ -25,16 +25,26 @@ sd.resolution = (1200, 600)
 #puz_param(point = point, step = 5)
 
 # Нарисовать 10 пузырьков в ряд
+
+#def puz_param(point):
+#    radius = 50
+#    for _ in range(3):
+#        sd.circle(point, radius=radius)
+#for x in range (100, 1001, 100):
+#    point = sd.get_point(x = x, y = 100)
+#    puz_param(point = point)
+
+# Нарисовать три ряда по 10 пузырьков
 # TODO здесь ваш код
 def puz_param(point):
     radius = 50
     for _ in range(3):
         sd.circle(point, radius=radius)
+
 for x in range (100, 1001, 100):
-    point = sd.get_point(x = x, y = 100)
-    puz_param(point = point)
-# Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+    for y in range (100, 301, 100):
+        point = sd.get_point(x = x, y = y)
+        puz_param(point = point)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
